7440028096e42f8bc493c868adb59579747340df
tayloranderson <tba964902@gmail.com>
2026-06-15 02:56:46 -0700

    Version: 1

---
+++ Encounter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Taylor Anderson on 6/15/26.
+//
+
+#include "Encounter.h"
---
+++ Encounter.h
@@ -0,0 +1,8 @@
+//
+// Created by Taylor Anderson on 6/15/26.
+//
+
+#ifndef ARMAGESTA_ENCOUNTER_H
+#define ARMAGESTA_ENCOUNTER_H
+
+#endif //ARMAGESTA_ENCOUNTER_H
