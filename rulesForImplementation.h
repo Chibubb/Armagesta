9062a6f5a1fd6f61a5cd28bf9c512709aad908c7
tayloranderson <tba964902@gmail.com>
2026-06-18 18:31:38 -0700

    Version: 13

---
+++ rulesForImplementation.h
@@ -0,0 +1,16 @@
+//
+// Created by Taylor Anderson on 6/18/26.
+//
+
+#ifndef ARMAGESTA_RULESFORIMPLEMENTATION_H
+#define ARMAGESTA_RULESFORIMPLEMENTATION_H
+
+//Implementing A new Biome
+//Make a new biome template in EVENTS.H
+//Place it on the map in PLAYERDATABASE.H
+//Update InteractWithWorld in MAIN.CPP
+
+//Implementing A new Encounter
+//Make a new encounter template in ENCOUNTER.H
+
+#endif //ARMAGESTA_RULESFORIMPLEMENTATION_H
